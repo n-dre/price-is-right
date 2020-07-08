8a11c700f2bd5f4c50b59119edf6f828c1e3e5b6
n-dre <65468451+n-dre@users.noreply.github.com>
2020-07-07 20:01:04 -0400

    Mgs

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-function calculateTotal(items) {
-  let newItems = [calculateTotal] 
+const items = [{
+  id: 1, price: 19.95, discount: .12, quantity: 2,
+},{
+  id: 2, price: 12.95, discount: .10, quantity: 1,
+}]
 
-  for(let i = 0; i < items.length; i++) {
-    newItems.push(items[i])
-  }
-
-  return total
+function hasPrice(item) {
+  return item.price > 0
 }
 
-const items = [
-  {id: 1, price: 19.95, discount: .12, quantity: 2},
-  {id: 2, price: 12.95, discount: .10, quantity: 1}
-]
-const total = items.map((items) => function(calculateTotal) {
-  return total - 2
-})
+function objectTotal(item) {
+  const unformattedTotal = item.price * (1 - item.discount) * item.quantity
+  const total = parseInt(unformattedTotal * 100) / 100
 
-console.log(total)
+  return { ...item, total }
+}
+
+function calculateTotal(items) {
+  items = items.map(objectTotal)
+
+  return items
+}
 
 module.exports = calculateTotal
